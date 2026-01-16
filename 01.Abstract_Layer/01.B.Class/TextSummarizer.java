a4faf1258fb624a2f53adacf62ee9935c8018708
dev1week <rondo2860@gmail.com>
2026-01-17 00:58:01 +0900

    refactor : class separation using dependency injection

--- 01.Abstract_Layer/01.B.Class/TextSummarizer.java
+++ 01.Abstract_Layer/01.B.Class/TextSummarizer.java
@@ -1,11 +1,51 @@
 class TextSummarizer{
+    private final ParagraphFinder paragraphFinder;
+    private final TextImportanceScorer textImportanceScorer;
 
-    String summarizeText(String text) {
-        return splitIntoParagraphs(text)
-                .filter(paragraph -> calculateImportanceScore(paragraph) > 0.5)
-                .join("\n");
+    public TextSummarizer(ParagraphFinder paragraphFinder, TextImportanceScorer textImportanceScorer) {
+        this.paragraphFinder = paragraphFinder;
+        this.textImportanceScorer = textImportanceScorer;
     }
 
+    String summarizeText(String text) {
+        return paragraphFinder.find(text)
+                .filter(paragraph ->importanceScorer.isImportant(paragraph))
+                .join("\n");
+    }
+}
+
+
+class ParagraphFinder{
+
+    private List<String> find(String text) {
+        List<String> paragraphs = new ArrayList<>();
+
+        Int start = detectParagraphStartOffset(text,0 );
+        while(start != null){
+            Int end = detectParagraphEndOffset(text,start);
+            if(end == null){
+                break;;
+            }
+            paragraphs.add(text.substring(start, end));
+            start = detectParagraphStartOffset(text,end);
+        }
+        return paragraphs;
+    }
+
+    private Int detectParagraphStartOffset(String text, Int currentOffset) {
+        //구현 중략 
+    }
+
+    private Int detectParagraphEndOffset(String text, Int currentOffset) {
+        //구현 중략 
+    }
+}
+
+class TextImportanceScorer{
+
+    public Boolean isImportant(String text) {
+        return calculateImportanceScore(text) > 0.5;
+    }
 
     private double calculateImportanceScore(String paragraph) {
         List<String> nouns = extractNouns(paragraph);
@@ -26,31 +66,6 @@ class TextSummarizer{
     }
 
     private List<String> extractAdjectives(String paragraph) {
-        //구현 중략 
-    }
-
-
-    private List<String> splitIntoParagraphs(String text) {
-        List<String> paragraphs = new ArrayList<>();
-
-        Int start = detectParagraphStartOffset(text,0 );
-        while(start != null){
-            Int end = detectParagraphEndOffset(text,start);
-            if(end == null){
-                break;;
-            }
-            paragraphs.add(text.substring(start, end));
-            start = detectParagraphStartOffset(text,end);
-        }
-        return paragraphs;
-
-    }
-
-    private Int detectParagraphStartOffset(String text, Int currentOffset) {
-        //구현 중략 
-    }
-
-    private Int detectParagraphEndOffset(String text, Int currentOffset) {
         //구현 중략 
     }
 
